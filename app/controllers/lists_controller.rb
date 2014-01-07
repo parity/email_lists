59245fd15076cdf74476580bec254947a522bcc3
Jan Hovancik <conta.srdr@gmail.com>
2014-01-07 18:23:11 +0530

    Edit list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,6 +17,20 @@ class ListsController < ApplicationController
 		@list = List.find(params[:id])
 	end
 
+	def edit
+		@list = List.find(params[:id])
+	end	
+
+	def update
+		@list= List.find(params[:id])
+		if @list.update_attributes(list_params)
+			flash[:success] = "List Information updated, hence new profile"
+			redirect_to @list
+		else
+			render 'edit'
+		end
+	end
+
 	private
 
 		def list_params
--- spec/requests/list_pages_spec.rb
+++ spec/requests/list_pages_spec.rb
@@ -28,6 +28,38 @@ describe "List pages" do
 			end
 		end
 	end
+
+	describe "edit list" do
+		let(:list) { FactoryGirl.create(:list)}
+		before {visit edit_list_path(list)}
+
+		describe "with invalid information" do
+			before do 
+				fill_in "Name", with: ""
+				click_button "Edit List"
+			end
+			it {should have_selector('div.alert.alert-danger')}
+
+		end
+
+		describe "with good infos" do
+			let(:new_name) {"name"}
+			let(:new_description) {"description"}
+			let(:new_address) {"new@example.com"}
+			before do
+				fill_in "Name", with: new_name
+				fill_in "Description",with: new_description
+				fill_in "Address", with: new_address
+				click_button "Edit List"
+			end
+		specify { expect(list.reload.name).to  eq new_name }
+      	specify { expect(list.reload.description).to eq new_description }
+      	specify { expect(list.reload.address).to  eq new_address}
+      end
+
+	end
+
+
 end
 
 
