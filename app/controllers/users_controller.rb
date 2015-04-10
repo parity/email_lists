ba91c46ac70e0b63b0669fc74b540fab01803dd1
Sandeep Maurya <snmplanet@gmail.com>
2015-04-10 18:49:51 +0530

    fixed some issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
  	def create
 	    @user = User.new(user_params)
 	    if @user.save 	    	
-	    	flash[:success] = "New User Created, hence the User Information."
+	    	flash[:success] = "New User Created, hence the User Information. User password is: firstname_1234567"
 	    	redirect_to @user
 	    else
 	    	render 'new'
